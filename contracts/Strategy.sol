4e533c2b4851e8e65f0aef9e121a40119a1150c0
bitnician <b3hzad@me.com>
2021-05-21 20:22:02 +0430

    Adding withdrawFromProtocol modifying withdrawFromProtocol Add commnets for functions

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -17,9 +17,13 @@ contract Strategy is BaseStrategy {
   using Address for address;
   using SafeMath for uint256;
 
+  //The strategy will use this protocol for yield farming
   ISampleProtocol public sampleProtocol;
+  //After sending `want` to protocol, protocol will send us LP token.
   IERC20 public lpToken;
+  //We use uniswap just for getting the eth value in `want`. we assume there is a eth/want pair in uniswap.
   IUniswapRouter public uniswapRouter;
+  //The address of global weth
   address public weth;
 
   constructor(
@@ -34,13 +38,17 @@ contract Strategy is BaseStrategy {
     uniswapRouter = IUniswapRouter(_uniswapRouter);
     weth = _weth;
 
+    //Protocol needs to call transferFrom() to transfer `want` from strategy to the protocol
     want.safeApprove(address(sampleProtocol), uint256(-1));
-    lpToken.safeApprove(address(this), uint256(-1));
+
+    //Protocol needs to call transferFrom() to transfer LP token from strategy to the protocol
+    lpToken.safeApprove(address(sampleProtocol), uint256(-1));
   }
 
   function name() external view override returns (string memory) {
     return 'StrategySampleProtocolToken';
   }
+
 
   function estimatedTotalAssets() public view override returns (uint256) {
     uint256 lpBalance = lpToken.balanceOf(address(this));
@@ -75,10 +83,15 @@ contract Strategy is BaseStrategy {
     _profit = 0;
     _loss = 0;
 
+
+    //Total debt = Total outstanding debt that Strategy has 
     uint256 totalDebt = vault.strategies(address(this)).totalDebt;
+    //Total amount of `wants`, the current value of `want` + the profit of `wants`
     uint256 totalAsstes = estimatedTotalAssets();
+    //Current amount of `want`
     uint256 wantBalance = balanceOfWant();
 
+    //If the total wants is greater than total debt, we are obviously in profit.
     if (totalAsstes > totalDebt) {
       _profit = totalAsstes.sub(totalDebt);
     } else {
@@ -88,21 +101,17 @@ contract Strategy is BaseStrategy {
     //Profit from liquidity mining + _debtOutstanding(the amount that vault needs back from the strategy)
     uint256 toFree = _debtOutstanding.add(_profit);
 
+    //If the toFree is greater than the current balance of `want`, we have to withdraw some amount of `want` from protocol   
     if (toFree > wantBalance) {
-      //`toFree` is greater than `want` balance, so we need to wihdraw some `want` from protocol
+
+      //`toFree` is greater than `want` balance, so we need calculate how much `want` we need to withdraw
       uint256 withdrawalAmount = toFree.sub(wantBalance);
 
-      //calculate how much LP token should send to protocol to withdraw desired `want`
-      uint256 totalLpTokens = lpToken.balanceOf(address(this));
-      uint256 desireLpToken = uint256(withdrawalAmount).mul(totalLpTokens).div(totalAsstes) + 1;
+     (uint256 _liquidatedAmount, )  = liquidatePosition(withdrawalAmount);
 
-      //Send Lp token to protocol and recieve back the want
-      sampleProtocol.removeLiquidity(desireLpToken);
 
-      uint256 newWantBalance = balanceOfWant();
-      uint256 freedAmountFromProtocol = newWantBalance.sub(wantBalance);
-
-      uint256 withdrawalLoss = toFree.sub(freedAmountFromProtocol);
+    // loss in the case _liquidatedAmount is  less than amount that needs to be freed
+    uint256 withdrawalLoss = _liquidatedAmount < withdrawalAmount ? withdrawalAmount.sub(_liquidatedAmount) : 0;
 
       if (withdrawalLoss < _profit) {
         _profit = _profit.sub(withdrawalLoss);
@@ -124,28 +133,57 @@ contract Strategy is BaseStrategy {
   }
 
   function adjustPosition(uint256 _debtOutstanding) internal override {
+    //1. We check the amout of `want` in strategy
     uint256 wantBalance = balanceOfWant();
+
+    //2. check to see if the amount is greater than the amount that vault needs back from the strategy
     if (wantBalance > _debtOutstanding) {
       uint256 excessWant = wantBalance.sub(_debtOutstanding);
 
+    //3. we reinvest the excess amount of want
       sampleProtocol.provideLiquidity(excessWant);
     }
   }
+
+
+
 
   function liquidatePosition(uint256 _amountNeeded)
     internal
     override
     returns (uint256 _liquidatedAmount, uint256 _loss)
   {
-    uint256 wantBalance = balanceOfWant();
 
-    if (_amountNeeded > wantBalance) {
-      _liquidatedAmount = wantBalance;
-      _loss = _amountNeeded.sub(wantBalance);
-    } else {
-      _liquidatedAmount = wantBalance.sub(_amountNeeded);
-      _loss = 0;
-    }
+       uint256 balanceOfWant = balanceOfWant(); 
+
+        if (balanceOfWant >= _amountNeeded) {
+            _liquidatedAmount = _amountNeeded;
+            _loss = 0;
+            withdrawFromProtocol(_liquidatedAmount);
+        } else {
+            _liquidatedAmount = balanceOfWant;
+            _loss = _amountNeeded.sub(balanceOfWant);
+             withdrawFromProtocol(_liquidatedAmount);
+        }
+
+
+  }
+
+
+  function withdrawFromProtocol(uint256 withdrawalAmountInWant)internal returns(uint256 freedAmountFromProtocol){
+      uint256 totalAsstes = estimatedTotalAssets();
+      uint256 wantBalance = balanceOfWant();
+
+       //calculate how much LP token should send to protocol to withdraw desired `want`
+      uint256 totalLpTokens = lpToken.balanceOf(address(this));
+      uint256 desireLpToken = uint256(withdrawalAmountInWant).mul(totalLpTokens).div(totalAsstes) + 1;
+
+      //Send Lp token to protocol and recieve back the want
+      sampleProtocol.removeLiquidity(desireLpToken);
+
+      //Here we calculate the real amount of `want` that has been withdrawn from protocol
+      uint256 newWantBalance = balanceOfWant();
+      freedAmountFromProtocol = newWantBalance.sub(wantBalance);
   }
 
   function prepareMigration(address _newStrategy) internal override {}
--- contracts/interfaces/ISampleProtocol.sol
+++ contracts/interfaces/ISampleProtocol.sol
@@ -2,13 +2,13 @@ pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 interface ISampleProtocol {
-  function deposit() external;
 
-  function provideLiquidity(uint256 underlyingAssetamount) external;
+  //This function allows us to provice Liquidity to the protocol
+  function provideLiquidity(uint256 underlyingAsset) external;
 
-  function removeLiquidity(uint256 lpTokenAmount) external;
+  //This function allows us to get back the underlying asset + profits
+  function removeLiquidity(uint256 lpToken) external;
 
-  function withdraw() external;
-
+  //This function will return the rewards that has been accumulated. (same as MasterChef contract of sushiswap)
   function calculateAccumulateFee(uint256 lpToken) external view returns (uint256);
 }
